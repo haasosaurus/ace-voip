dbc4aea3b7b57744f07ab46a26aefb39117919a3
Devon Kearns <dookie@kali.org>
2012-10-26 05:21:31 -0600

    Pre-build commit.

--- debian/ace.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ace package
-#
-0 4	* * *	root	[ -x /usr/bin/ace_maintenance ] && /usr/bin/ace_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ace):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ace" command="/usr/bin/ace"
